b0fe290649bb2c78925bf44bc2563197783fb67f
Deepesh Padala <33351122+DS3a@users.noreply.github.com>
2022-08-03 13:34:07 +0530

    add library for ros_o3d bridge

--- scripts/pcd_open3d.py
+++ scripts/pcd_open3d.py
@@ -6,14 +6,14 @@ from rclpy.node import Node
 import sensor_msgs.msg as sensor_msgs
 from sensor_msgs.msg import PointCloud2, PointField
 
-from traversability_layer import pcl_filtering
+from traversability_layer import pcl_filtering, ros_o3d_bridge
 import open3d as o3d
 import numpy as np
-import sys
-from collections import namedtuple
-import ctypes
-import math
-import struct
+#import sys
+#from collections import namedtuple
+#import ctypes
+#import math
+#import struct
 
 class PCDListener(Node):
 
@@ -34,7 +34,16 @@ class PCDListener(Node):
             self.listener_callback,      # Function to call
             10                          # QoS
         )
-
+        self.travsersible_points_publisher = self.create_publisher(
+            PointCloud2,
+            '/traversible_points',
+            10
+        )
+        self.travsersible_points_publisher = self.create_publisher(
+            PointCloud2,
+            '/traversible_points',
+            10
+        )
 
     def listener_callback(self, msg):
         # pcl_data = util.convert_pcl(msg)
@@ -52,8 +61,8 @@ class PCDListener(Node):
         #tmp_pcl.colors = pc_c3d
         #return tmp_pcl
 
-        pcd_as_numpy_array = np.array(list(read_points(msg)))[:, :3]
-        pcl_filtering.filter_z(pcd_as_numpy_array, 0.05)
+        pcd_as_numpy_array = np.array(list(ros_o3d_bridge.read_points(msg)))[:, :3]
+        #pcl_filtering.filter_z(pcd_as_numpy_array, 1, 0.05)
 
 
         # The rest here is for visualization.
@@ -65,94 +74,10 @@ class PCDListener(Node):
             self.vis.poll_events()
             self.vis.update_renderer()
 
-_DATATYPES = {}
-_DATATYPES[PointField.INT8]    = ('b', 1)
-_DATATYPES[PointField.UINT8]   = ('B', 1)
-_DATATYPES[PointField.INT16]   = ('h', 2)
-_DATATYPES[PointField.UINT16]  = ('H', 2)
-_DATATYPES[PointField.INT32]   = ('i', 4)
-_DATATYPES[PointField.UINT32]  = ('I', 4)
-_DATATYPES[PointField.FLOAT32] = ('f', 4)
-_DATATYPES[PointField.FLOAT64] = ('d', 8)
-
-
-def read_points(cloud, field_names=None, skip_nans=False, uvs=[]):
-    """
-    Read points from a L{sensor_msgs.PointCloud2} message.
-    @param cloud: The point cloud to read from.
-    @type  cloud: L{sensor_msgs.PointCloud2}
-    @param field_names: The names of fields to read. If None, read all fields. [default: None]
-    @type  field_names: iterable
-    @param skip_nans: If True, then don't return any point with a NaN value.
-    @type  skip_nans: bool [default: False]
-    @param uvs: If specified, then only return the points at the given coordinates. [default: empty list]
-    @type  uvs: iterable
-    @return: Generator which yields a list of values for each point.
-    @rtype:  generator
-    """
-    assert isinstance(cloud, PointCloud2), 'cloud is not a sensor_msgs.msg.PointCloud2'
-    fmt = _get_struct_fmt(cloud.is_bigendian, cloud.fields, field_names)
-    width, height, point_step, row_step, data, isnan = cloud.width, cloud.height, cloud.point_step, cloud.row_step, cloud.data, math.isnan
-    unpack_from = struct.Struct(fmt).unpack_from
-
-    if skip_nans:
-        if uvs:
-            for u, v in uvs:
-                p = unpack_from(data, (row_step * v) + (point_step * u))
-                has_nan = False
-                for pv in p:
-                    if isnan(pv):
-                        has_nan = True
-                        break
-                if not has_nan:
-                    yield p
-        else:
-            for v in range(height):
-                offset = row_step * v
-                for u in range(width):
-                    p = unpack_from(data, offset)
-                    has_nan = False
-                    for pv in p:
-                        if isnan(pv):
-                            has_nan = True
-                            break
-                    if not has_nan:
-                        yield p
-                    offset += point_step
-    else:
-        if uvs:
-            for u, v in uvs:
-                yield unpack_from(data, (row_step * v) + (point_step * u))
-        else:
-            for v in range(height):
-                offset = row_step * v
-                for u in range(width):
-                    yield unpack_from(data, offset)
-                    offset += point_step
-
-
-def _get_struct_fmt(is_bigendian, fields, field_names=None):
-    fmt = '>' if is_bigendian else '<'
-
-    offset = 0
-    for field in (f for f in sorted(fields, key=lambda f: f.offset) if field_names is None or f.name in field_names):
-        if offset < field.offset:
-            fmt += 'x' * (field.offset - offset)
-            offset = field.offset
-        if field.datatype not in _DATATYPES:
-            print('Skipping unknown PointField datatype [%d]' % field.datatype, file=sys.stderr)
-        else:
-            datatype_fmt, datatype_length = _DATATYPES[field.datatype]
-            fmt    += field.count * datatype_fmt
-            offset += field.count * datatype_length
-
-    return fmt
-
-
 def main(args=None):
     # Boilerplate code.
     rclpy.init(args=args)
-    pcd_listener = PCDListener()
+    pcd_listener = PCDListener(debug=True)
     rclpy.spin(pcd_listener)
 
     # Destroy the node explicitly
--- traversability_layer/pcl_filtering.py
+++ traversability_layer/pcl_filtering.py
@@ -1,11 +1,19 @@
 import open3d as o3d
 import numpy as np
+from traversability_layer import ros_o3d_bridge
 
-def filter_z(pcl_data, window = 0.05):
+def filter_z(pcl_data, base=0, window = 0.5):
     np_array = np.asarray(pcl_data)
     o3d_pcd = o3d.geometry.PointCloud(
-        o3d.utility.Vector3dVector(np_array))
+        o3d.utility.Vector3dVector(np_array)) #`voxel_down_sample(voxel_size=0.05)` if it's too sluggish
 
     ## From Open3D to numpy
-    ##np_points = np.asarray(pcd.points)
-    print(np_array.shape)
+    np_points = np.asarray(o3d_pcd.points)
+    np_filter = []
+    for point in np_points:
+        if (base - window) < (point[2] - base) < (base + window):
+            np_filter.append(False)
+        else:
+            np_filter.append(True)
+
+    return np_points[np_filter]
