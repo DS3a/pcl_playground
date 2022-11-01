6d00211383a12949655dd09a6058b5ed3c9a7b4a
DS3a <33351122+DS3a@users.noreply.github.com>
2022-11-01 23:02:13 +0530

    removd left shit

--- src/pcl_rescale.cpp
+++ src/pcl_rescale.cpp
@@ -16,48 +16,14 @@
 #include "pcl/common/pca.h"
 #include "pcl/common/transforms.h"
 #include "pcl/common/common.h"
-// #include "../include/rectangle_filter.hpp"
 #include <boost/foreach.hpp>
 
 
-#define POINTS_TOPIC "/velodynes/left/points"
 #define CENTER_POINTS_TOPIC "/velodynes/center/points"
 #define ROBOT_HEIGHT 2.8
 #define POINT_TYPE pcl::PointXYZ
 
 using namespace std::chrono_literals;
-
-
-bool initiated = false;
-
-void rescaleCloudsPrereq(pcl::PointCloud<pcl::PointXYZ>::Ptr& goldenCloud, pcl::PointCloud<pcl::PointXYZ>::Ptr& sampleCloud) {
-
-  //analyze golden cloud
-    pcl::PCA<pcl::PointXYZ> pcaGolden;
-    pcaGolden.setInputCloud(goldenCloud);
-    Eigen::Matrix3f goldenEVs_Dir = pcaGolden.getEigenVectors();
-    Eigen::Vector4f goldenMidPt = pcaGolden.getMean();
-    Eigen::Matrix4f goldenTransform = Eigen::Matrix4f::Identity();
-    goldenTransform.block<3, 3>(0, 0) = goldenEVs_Dir;
-    goldenTransform.block<4, 1>(0, 3) = goldenMidPt;
-    pcl::PointCloud<pcl::PointXYZ>::Ptr orientedGolden(new pcl::PointCloud<pcl::PointXYZ>);
-    pcl::transformPointCloud(*goldenCloud, *orientedGolden, goldenTransform.inverse());
-    pcl::PointXYZ goldenMin, goldenMax;
-    pcl::getMinMax3D(*orientedGolden, goldenMin, goldenMax);
-    //analyze sample cloud
-
-    pcl::PCA<pcl::PointXYZ> pcaSample;
-    pcaSample.setInputCloud(sampleCloud);
-    static Eigen::Matrix3f sampleEVs_Dir = pcaSample.getEigenVectors();
-    static Eigen::Vector4f sampleMidPt = pcaSample.getMean();
-
-    Eigen::Matrix4f sampleTransform = Eigen::Matrix4f::Identity();
-    sampleTransform.block<3, 3>(0, 0) = sampleEVs_Dir;
-    sampleTransform.block<4, 1>(0, 3) = sampleMidPt;
-    pcl::PointCloud<pcl::PointXYZ>::Ptr orientedSample(new pcl::PointCloud<pcl::PointXYZ>);
-    pcl::transformPointCloud(*sampleCloud, *orientedSample, sampleTransform.inverse());
-
-}
 
 void rescaleClouds(pcl::PointCloud<pcl::PointXYZ>::Ptr& sampleCloud) {
     double xScale = 2.0;
@@ -88,9 +54,6 @@ class PclFilter : public rclcpp::Node
     PclFilter()
     : Node("pcl_rescaler"), count_(0)
     {
-      pcl_subscriber = this->create_subscription<sensor_msgs::msg::PointCloud2>(
-        POINTS_TOPIC, 10, std::bind(&PclFilter::pcl_callback, this, std::placeholders::_1));
-
       center_pcl_subscriber = this->create_subscription<sensor_msgs::msg::PointCloud2>(
         CENTER_POINTS_TOPIC, 10, std::bind(&PclFilter::center_pcl_callback, this, std::placeholders::_1));
 
@@ -111,17 +74,6 @@ class PclFilter : public rclcpp::Node
     }
 
   private:
-    void pcl_callback(const sensor_msgs::msg::PointCloud2::SharedPtr msg) {
-      pcl::fromROSMsg(*msg, *cloud);
-      grid.filter(*cloud);
-      if (!initiated) {
-        initiated = true;
-        if ((cloud->size() > 0) && (center_cloud->size() > 0)) {
-          rescaleCloudsPrereq(cloud, center_cloud);
-        }
-      }
-    }
-
     void center_pcl_callback(const sensor_msgs::msg::PointCloud2::SharedPtr msg) {
       pcl::fromROSMsg(*msg, *center_cloud);
       rescaleClouds(center_cloud);
